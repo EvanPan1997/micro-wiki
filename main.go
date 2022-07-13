81b6b3da5461b0a4ff357b6346c04384646f1bfb
EvanPan1997 <3578313864@qq.com>
2022-07-13 17:26:44 +0800

    项目重构

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	_ = r.Run(":9090")
+}
--- util/base64.go
+++
@@ -1,7 +0,0 @@
-package util
-
-import "encoding/base64"
-
-func EncodeToBase64(b []byte) string {
-	return base64.StdEncoding.EncodeToString(b)
-}
