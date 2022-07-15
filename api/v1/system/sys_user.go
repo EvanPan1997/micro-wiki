46adaf5340434fe06c11b0c13839b2ab7a14b06e
EvanPan1997 <3578313864@qq.com>
2022-07-15 09:45:14 +0800

    add: 用户注册接口数据打通, 新增uuid

--- api/v1/system/sys_user.go
+++ api/v1/system/sys_user.go
@@ -3,15 +3,16 @@ package system
 import (
 	"github.com/gin-gonic/gin"
 	"micro-wiki/model/common/response"
+	"micro-wiki/model/system"
 )
 
 type UserApi struct {
 }
 
 func (s *UserApi) Register(c *gin.Context) {
-	// 接收请求体
-
 	// 插入记录到wiki_user表中
+	var wikiUser *system.WikiUser = new(system.WikiUser)
+	wikiUser.RegisterFormToWikiUser(c)
 
-	response.OkWithMessage("Register Success", c)
+	response.OkWithDetail(wikiUser, "Register Success", c)
 }
