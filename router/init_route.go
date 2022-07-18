5407aa8a8fb355d07bd790670c510d6a1cfb96ac
EvanPan1997 <3578313864@qq.com>
2022-07-18 13:41:16 +0800

    add: 跨域中间件, 处理跨域请求方法变成OPTIONS的问题

--- router/init_route.go
+++ router/init_route.go
@@ -2,11 +2,13 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	"micro-wiki/midderware/common"
 	"micro-wiki/router/v1/system"
 )
 
 func InitRoute(r *gin.Engine) {
 	// 初始化路由
+	r.Use(common.Cors())
 	v1 := r.Group("/v1")
 
 	var userRouter system.UserRouter
