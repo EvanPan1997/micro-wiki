0b3388e9ca2bb73d7d7e883525f76f990702eb58
EvanPan1997 <3578313864@qq.com>
2022-07-19 11:32:23 +0800

    update: 更新gorm字段解析, 更新解析json数据请求的数据绑定

--- model/system/wiki_user.go
+++ model/system/wiki_user.go
@@ -3,6 +3,7 @@ package system
 import (
 	"github.com/gin-gonic/gin"
 	"micro-wiki/utils"
+	"net/http"
 	"time"
 )
 
@@ -12,50 +13,59 @@ var (
 
 type WikiUser struct {
 	// 数据ID
-	DataID string
+	DataID string `gorm:"primaryKey" column:"data_id"`
 	// 用户信息
-	UserID            string
-	Username          string
-	Password          string
-	RoleID            string
-	Status            string
-	IsLocked          string
-	LastLoginTime     time.Time
-	LastLogoutTime    time.Time
-	LoginIP           string
-	PwdErrorCount     int
-	TotalPwdErr       int
-	LastChangePwdTime time.Time
-	AccessToken       string
+	UserID            string    `column:"user_id"`
+	Username          string    `column:"username"`
+	Password          string    `column:"password"`
+	RoleID            string    `column:"role_id"`
+	Status            string    `column:"status"`
+	IsLocked          string    `column:"is_locked"`
+	LastLoginTime     time.Time `column:"last_login_time"`
+	LastLogoutTime    time.Time `column:"last_logout_time"`
+	LoginIP           string    `column:"login_ip"`
+	PwdErrorCount     int       `column:"pwd_error_count"`
+	TotalPwdErr       int       `column:"total_pwd_err"`
+	LastChangePwdTime time.Time `column:"last_change_pwd_time"`
+	AccessToken       string    `column:"access_token"`
 	// 系统维护信息
-	CreateUser string
-	CreateAt   time.Time
-	ChangeUser string
-	ChangeAt   time.Time
-	DeleteUser string
-	DeleteAt   time.Time
+	CreateUser string    `column:"create_user"`
+	CreateAt   time.Time `column:"create_at"`
+	ChangeUser string    `column:"change_user"`
+	ChangeAt   time.Time `column:"change_at"`
+	DeleteUser string    `column:"delete_user"`
+	DeleteAt   time.Time `column:"delete_at"`
 	// 补充字段
-	remarks  string
-	Reserve1 string
-	Reserve2 string
-	Reserve3 string
-	Reserve4 string
-	Reserve5 string
+	remarks  string `column:"remarks"`
+	Reserve1 string `column:"reserve1"`
+	Reserve2 string `column:"reserve2"`
+	Reserve3 string `column:"reserve3"`
+	Reserve4 string `column:"reserve4"`
+	Reserve5 string `column:"reserve5"`
+}
+
+type RegisterReq struct {
+	Username string `form:"username" json:"username" binding:"required"`
+	Operator string `form:"operator" json:"operator" binding:"required"`
 }
 
 /* 将用户注册表单, 转换为WikiUser实例 */
-//func (wikiUser *WikiUser) RegisterFormToWikiUser(form map[string]string) {
 func (wikiUser *WikiUser) RegisterFormToWikiUser(c *gin.Context) {
-
+	// 提供hash值作为本结构体的id
 	ids := utils.GenUuidV4()
-	username := c.PostForm("username")
-	operator := c.PostForm("operator")
 
+	// bind json
+	var registerReq RegisterReq
+	if err := c.BindJSON(&registerReq); err != nil {
+		c.AbortWithStatusJSON(
+			http.StatusInternalServerError,
+			gin.H{"error": err.Error()})
+	}
+
+	// 基本信息
 	wikiUser.DataID = ids
 	wikiUser.UserID = ids
-	//wikiUser.Username 		= form["username"]
-	wikiUser.Username = username
-
+	wikiUser.Username = registerReq.Username
 	wikiUser.Password = utils.BcryptHash(InitPassword)
 	wikiUser.RoleID = "User"
 	wikiUser.Status = "1"
@@ -63,10 +73,10 @@ func (wikiUser *WikiUser) RegisterFormToWikiUser(c *gin.Context) {
 	wikiUser.PwdErrorCount = 0
 	wikiUser.TotalPwdErr = 0
 
-	//wikiUser.CreateUser 	= form["operator"]
-	wikiUser.CreateUser = operator
+	// 创建信息
+	wikiUser.CreateUser = registerReq.Operator
 	wikiUser.CreateAt = time.Now()
-	//wikiUser.ChangeUser 	= form["operator"]
-	wikiUser.ChangeUser = operator
+	// 修改信息
+	wikiUser.ChangeUser = registerReq.Operator
 	wikiUser.ChangeAt = time.Now()
 }
