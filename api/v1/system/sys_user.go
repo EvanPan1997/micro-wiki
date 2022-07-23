8900d7c4f24cb72170fff420312c22341bcb34f7
EvanPan1997 <3578313864@qq.com>
2022-07-23 20:46:59 +0800

    update: 新增防止插入重复用户的处理逻辑

--- api/v1/system/sys_user.go
+++ api/v1/system/sys_user.go
@@ -2,6 +2,7 @@ package system
 
 import (
 	"github.com/gin-gonic/gin"
+	"log"
 	"micro-wiki/global"
 	"micro-wiki/model/common/response"
 	"micro-wiki/model/system"
@@ -16,6 +17,20 @@ func (s *UserApi) Register(c *gin.Context) {
 	wikiUser.RegisterFormToWikiUser(c)
 
 	db := global.MW_DB
+	// 查询是否有重复用户
+	result := db.Table("wiki_user").Take(&system.WikiUser{Username: wikiUser.Username})
+	if result.Error != nil {
+		log.Println(result.Error)
+		response.Fail(c)
+	}
+	if result.RowsAffected > 0 {
+		log.Printf("用户:%s, 已存在", wikiUser.Username)
+		response.FailWithMessage("该用户已存在", c)
+		// 返回不再继续, 否则会继续执行插入
+		return
+	}
+
+	// 插入数据库
 	db.Table("wiki_user").Create(&wikiUser)
 
 	response.OkWithMessage("Register Success", c)
