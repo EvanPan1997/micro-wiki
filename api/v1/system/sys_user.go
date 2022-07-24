0ecc822e180cbe4866dbfc06d19706acde8f36d5
EvanPan1997 <3578313864@qq.com>
2022-07-24 20:55:10 +0800

    add: 用户详细信息逻辑

--- api/v1/system/sys_user.go
+++ api/v1/system/sys_user.go
@@ -21,7 +21,7 @@ func (s *UserApi) Register(c *gin.Context) {
 	result := db.Table("wiki_user").Take(&system.WikiUser{Username: wikiUser.Username})
 	if result.Error != nil {
 		log.Println(result.Error)
-		response.Fail(c)
+		response.FailWithMessage("用户注册失败", c)
 	}
 	if result.RowsAffected > 0 {
 		log.Printf("用户:%s, 已存在", wikiUser.Username)
@@ -34,4 +34,30 @@ func (s *UserApi) Register(c *gin.Context) {
 	db.Table("wiki_user").Create(&wikiUser)
 
 	response.OkWithMessage("Register Success", c)
+	return
+}
+
+func (s *UserApi) QueryUserDetail(c *gin.Context) {
+	userId := c.Query("user_id")
+
+	// 确认是否有权限查询
+
+	// 查询用户信息
+	db := global.MW_DB
+	var wikiUser system.WikiUser
+	result := db.Table("wiki_user").Find(&wikiUser, &system.WikiUser{UserID: userId})
+	// 判断是否报错
+	if result.Error != nil {
+		log.Println(result.Error)
+		response.FailWithMessage("用户查询失败", c)
+	}
+	// 判断是否查到用户
+	if result.RowsAffected == 0 {
+		log.Printf("UserID:%s, 不存在", userId)
+		response.FailWithMessage("该用户不存在", c)
+		return
+	}
+
+	response.OkWithMessage("Register Success", c)
+	return
 }
